06374fed1915ac80a4319f08828c0dd832eb0a20
Bo Ott <boott705@gmail.com>
2023-01-27 15:16:01 -0600

    Started on the game.

--- DragonGameClone.py
+++ DragonGameClone.py
@@ -1,4 +1,15 @@
+import random
+# Main function
 def main():
+    # Tests main
     print("This is a dragon game.")
+
+def dungeonShuffle():
+    rooms = ["START", "DRAGON", "SWORD", "KEY", "SHIELD", 6, 
+    7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+    17, 18, 19, 20, 21, 22, 23, 24, 25]
+    rooms
+
+# Creates main
 if __name__=="__main__":
     main()
