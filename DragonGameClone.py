4a77eec9dc499f5081f8b03b704d92a0c7a201da
boott705 <122580948+boott705@users.noreply.github.com>
2023-03-01 15:19:02 -0600

    Update DragonGameClone.py

--- DragonGameClone.py
+++ DragonGameClone.py
@@ -28,6 +28,61 @@ class Dungeon:
         []
     ]
 
+    def create_branch(rooms, room_amount, new_room_probability, dungeon_map, good_dungeon, current_room_y, current_room_x, break_off_points):
+        if room_amount == 3:
+            return dungeon_map
+        while good_dungeon == False:
+            new_room_probability = room_amount
+            adjacent_rooms = []
+            if current_room_y + 1 < 5:
+                if dungeon_map[current_room_y + 1][current_room_x] == 0:
+                    adjacent_rooms.append(1)
+            if dungeon_map[current_room_y][current_room_x + 1] == 0 and current_room_x + 1 <= 5:
+                adjacent_rooms.append(2)
+            if dungeon_map[current_room_y - 1][current_room_x] == 0 and current_room_y - 1 >= 0:
+                adjacent_rooms.append(3)
+            if dungeon_map[current_room_y][current_room_x - 1] == 0 and current_room_x - 1 >= 0:
+                adjacent_rooms.append(4)
+            for room in adjacent_rooms:
+                new_room = random.choice(range(new_room_probability))
+                if new_room <= new_room_probability[new_room_probability//2]:
+                    next_room = random.choice(adjacent_rooms)
+                    if room == next_room and room == 1:
+                        room_amount += 1
+                        dungeon_map[current_room_y + 1][current_room_x] = rooms[0]
+                        rooms.pop(0)
+                        current_room_y = current_room_y + 1
+                        current_room_x = current_room_x
+                        rooms.remove()
+                        Dungeon.create_branch(rooms, room_amount, new_room_probability, dungeon_map, good_dungeon, current_room_y, current_room_x, break_off_points)
+                    if room == next_room and room == 2:
+                        room_amount += 1
+                        dungeon_map[current_room_y + 1][current_room_x] = rooms[0]
+                        rooms.pop(0)
+                        current_room_y = current_room_y + 1
+                        current_room_x = current_room_x
+                        rooms.remove()
+                        Dungeon.create_branch(rooms, room_amount, new_room_probability, dungeon_map, good_dungeon, current_room_y, current_room_x, break_off_points)
+                    if room == next_room and room == 3:
+                        room_amount += 1
+                        dungeon_map[current_room_y + 1][current_room_x] = rooms[0]
+                        rooms.pop(0)
+                        current_room_y = current_room_y + 1
+                        current_room_x = current_room_x
+                        rooms.remove()
+                        Dungeon.create_branch(rooms, room_amount, new_room_probability, dungeon_map, good_dungeon, current_room_y, current_room_x, break_off_points)
+                    if room == next_room and room == 4:
+                        room_amount += 1
+                        dungeon_map[current_room_y + 1][current_room_x] = rooms[0]
+                        rooms.pop(0)
+                        current_room_y = current_room_y + 1
+                        current_room_x = current_room_x
+                        rooms.remove()
+                        Dungeon.create_branch(rooms, room_amount, new_room_probability, dungeon_map, good_dungeon, current_room_y, current_room_x, break_off_points)
+
+
+
+
     def generate(self):
         dungeon_map = [
         [0, 0, 0, 0, 0],
@@ -52,23 +107,9 @@ class Dungeon:
         room_amount = 1
         current_room_y = starting_room_y
         current_room_x = starting_room_x
-        while good_dungeon == False:
-            new_room_probability = 1
-            adjacent_rooms = []
-            if dungeon_map[current_room_y + 1][current_room_x] == 0:
-                adjacent_rooms.append(1)
-            if dungeon_map[current_room_y][current_room_x + 1] == 0:
-                adjacent_rooms.append(2)
-            if dungeon_map[current_room_y - 1][current_room_x] == 0:
-                adjacent_rooms.append(3)
-            if dungeon_map[current_room_y][current_room_x - 1] == 0:
-                adjacent_rooms.append(4)
-            for room in adjacent_rooms:
-                new_room = random.choice(range(new_room_probability))
-                if new_room == new_room_probability[0]:
-                    next_room = random.choice(adjacent_rooms)
-                    if room == next_room and room == 1:
-                        pass
+        new_room_probability = 1
+        break_off_points = []
+        dungeon_map = Dungeon.create_branch(rooms, room_amount, new_room_probability, dungeon_map, good_dungeon, current_room_y, current_room_x, break_off_points)
         print(dungeon_map)
         
     
