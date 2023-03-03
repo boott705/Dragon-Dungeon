729bb95ffb0682e7a56a606b067ac90a26fc3fdb
boott705 <122580948+boott705@users.noreply.github.com>
2023-03-02 22:49:38 -0600

    Finished dungeon generation

--- DragonGameClone.py
+++ DragonGameClone.py
@@ -1,98 +1,181 @@
-def create_branch(rooms, room_amount, dungeon_map, good_dungeon, current_room_y, current_room_x, unused_rooms, previous_x, previous_y):
-        print(dungeon_map)
-        possible_rooms = []
-        isBreakOff = False
-        if random.choice(range(100)) > 95:
-            isBreakOff = True
-        if room_amount == 16:
-            good_dungeon = True
-            return dungeon_map
-        while good_dungeon == False:
-            adjacent_rooms = []
-            if current_room_y + 1 < 5:
-                if dungeon_map[current_room_y + 1][current_room_x] == 0:
-                    adjacent_rooms.append(1)
-            if current_room_x + 1 < 5:
-                if dungeon_map[current_room_y][current_room_x + 1] == 0:
-                    adjacent_rooms.append(2)
-            if current_room_y - 1 >= 0:
-                if dungeon_map[current_room_y - 1][current_room_x] == 0:
-                    adjacent_rooms.append(3)
-            if current_room_x - 1 >= 0:
-                if dungeon_map[current_room_y][current_room_x - 1] == 0:
-                    adjacent_rooms.append(4)
-            for room in adjacent_rooms:
-                new_room = random.choice(range(99))
-                if new_room < 33:
-                    possible_room = random.choice(adjacent_rooms)
-                    if room == possible_room and room == 1:
-                        adjacent_rooms.remove(1)
-                        possible_rooms.append(room)
-                    if room == possible_room and room == 2:
-                        adjacent_rooms.remove(2)
-                        possible_rooms.append(room)
-                    if room == possible_room and room == 3:
-                        adjacent_rooms.remove(3)
-                        possible_rooms.append(room)
-                    if room == possible_room and room == 4:
-                        adjacent_rooms.remove(4)
-                        possible_rooms.append(room)
-            if len(possible_rooms) > 0 and isBreakOff == False:
-                next_room = random.choice(possible_rooms)
-                possible_rooms.remove(next_room)
-                for room in possible_rooms:
-                        unused_rooms.append(rooms[0])
-                        if room == 1:
-                            dungeon_map[current_room_y + 1][current_room_x] = rooms[0]
-                            rooms.pop(0)
-                        if room == 2:
-                            dungeon_map[current_room_y][current_room_x + 1] = rooms[0]
-                            rooms.pop(0)
-                        if room == 3:
-                            dungeon_map[current_room_y - 1][current_room_x] = rooms[0]
-                            rooms.pop(0)
-                        if room == 4:
-                            dungeon_map[current_room_y][current_room_x - 1] = rooms[0]
-                            rooms.pop(0)
-                if next_room == 1:
-                    room_amount += 1
-                    dungeon_map[current_room_y + 1][current_room_x] = rooms[0]
-                    rooms.pop(0)
-                    previous_x = current_room_x
-                    previous_y = current_room_y
-                    current_room_y = current_room_y + 1
-                    current_room_x = current_room_x                    
-                if next_room == 2:
-                    room_amount += 1
-                    dungeon_map[current_room_y][current_room_x + 1] = rooms[0]
-                    rooms.pop(0)
-                    previous_x = current_room_x
-                    previous_y = current_room_y
-                    current_room_y = current_room_y
-                    current_room_x = current_room_x + 1
-                if next_room == 3:
-                    room_amount += 1
-                    dungeon_map[current_room_y - 1][current_room_x] = rooms[0]
-                    rooms.pop(0)
-                    previous_x = current_room_x
-                    previous_y = current_room_y
-                    current_room_y = current_room_y - 1
-                    current_room_x = current_room_x
-                if next_room == 4:
-                    room_amount += 1
-                    dungeon_map[current_room_y][current_room_x - 1] = rooms[0]
-                    rooms.pop(0)
-                    previous_x = current_room_x
-                    previous_y = current_room_y
-                    current_room_y = current_room_y
-                    current_room_x = current_room_x - 1
-                Dungeon.create_branch(rooms, room_amount, dungeon_map, good_dungeon, current_room_y, current_room_x, unused_rooms, previous_x, previous_y)
+import random
+import math
+begin = 0
+total_rooms = 0
+i = 0
+# Main function
+def main():
+    # Tests main
+    print("This is a dragon game.")
+    Dungeon.generate()
+
+
+class Dungeon:
+    def __init__(self):
+        self.rooms = {}
+        self.starting_room = None
+        self.dragon_lair = None
+        self.sword_room = None
+        self.shield_room = None
+        self.key_room = None
+        self.roar_room = None
+        self.num_dungeons_generated = 0
+        
+    def toPositive(dungeon):
+        rowNum = 0
+        colNum = 0
+        for row in dungeon:
+            for col in row:
+                dungeon[rowNum][colNum] = abs(col)
+                colNum += 1
+            rowNum += 1
+            colNum = 0
+        return dungeon
+
+    def shuffleDungeon(arr2d): #Randomized rooms
+    #Shuffes entries of 2-d array arr2d, preserving shape.
+        reshape = [ ]
+        data = [ ]
+        iend = 0
+        for row in arr2d:
+            data.extend (row)
+            istart, iend = iend, iend+len (row)
+            reshape. append ((istart, iend))
+        random.shuffle(data)
+        return [data[istart: iend] for (istart, iend) in reshape]
+    
+    def checkDungeon(dungeon, all_valid):
+        global total_rooms
+        global begin
+        start_valid = False
+        connect_valid = False
+        start_room = random.choice(range(len(dungeon[4])))
+        begin = start_room
+        Dungeon.dfs(4, start_room, dungeon)
+        connect_valid = Dungeon.checkConnected(dungeon)
+        if connect_valid == True and start_valid == True:
+            all_valid = True
+        return all_valid
+    
+    def checkConnected(dungeon):
+        rooms_connected = 0
+        rowNum = 0
+        colNum = 0
+        for row in dungeon:
+            for col in row:
+                if dungeon[rowNum][colNum] < 0:
+                    rooms_connected += 1
+                colNum += 1
+            rowNum += 1
+            colNum = 0
+        print(rooms_connected)
+        if rooms_connected < 16:
+            return False
+        else:
+            return True
+
+    
+    def dfs(row, col, dungeon):
+        #CHECK BOUNDS
+        global total_rooms
+        if total_rooms < 16:
+            if row >= len(dungeon) and total_rooms <= 16:
+                return
+            if row < 0 and total_rooms <= 16:
+                return
+            if col >= len(dungeon[0]) and total_rooms <= 16:
+                return
+            if col < 0 and total_rooms <= 16:
+                return
+            #Check for 0
+            if dungeon[row][col] <= 0:
+                return
+            #WE'VE ALREADY BEEN HERE
+            dungeon[row][col] = -dungeon[row][col]
+            total_rooms += 1
+            directions = ['N', 'E', 'S', 'W']
+            random.shuffle(directions)
+            # Explore neighboring cells in a random order
+            for direction in directions:
+                if direction == 'N':
+                    Dungeon.dfs(row-1, col, dungeon)
+                elif direction == 'E':
+                    Dungeon.dfs(row, col+1, dungeon)
+                elif direction == 'S':
+                    Dungeon.dfs(row+1, col, dungeon)
+                elif direction == 'W':
+                    Dungeon.dfs(row, col-1, dungeon)
+        else:
+            return
+
+    def findBegin(dungeon, begin):
+        global i
+        for room in dungeon[4]:
+            if room > 0:
+                i += 1
+                print(i)
             else:
-                if len(unused_rooms) > 0:
-                    next_room = unused_rooms[0]
-                    for y in range(5):
-                        for x in range(5):
-                            if dungeon_map[y][x] == next_room:
-                                current_room_y = y
-                                current_room_x = x
-                    Dungeon.create_branch(rooms, room_amount, dungeon_map, good_dungeon, current_room_y, current_room_x, unused_rooms, previous_x, previous_y)
+                begin = dungeon[4][i]
+                break
+        print(i)
+        print(begin)
+        return begin
+    
+    def print_ascii(dungeon_map):
+        global begin
+        ascii_map = ""
+        for row in dungeon_map:
+            for room in row:
+                if room == begin:
+                    ascii_map += "╔════════╗"
+                elif room < 0:
+                    ascii_map += "║ ROOM   ║"
+                else:
+                    ascii_map += "║        ║"
+            ascii_map += "\n"
+            for room in row:
+                if room == begin:
+                   ascii_map += "║ Start  ║"
+                else:
+                    ascii_map += "║        ║"
+            ascii_map += "\n"
+            for room in row:
+                if room == begin:
+                    ascii_map += "║        ║"
+                elif room < 0:
+                    ascii_map += "║        ║"
+                else:
+                    ascii_map += "║        ║"
+            ascii_map += "\n"
+            for room in row:
+                if room == begin:
+                    ascii_map += "╚════════╝"
+                elif room < 0:
+                    ascii_map += "║        ║"
+                else:
+                    ascii_map += "║        ║"
+            ascii_map += "\n"
+        print(ascii_map)
+
+
+    def generate():
+        global begin
+        dungeon_map = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25]
+        ] 
+        all_valid = False
+        while all_valid == False:
+            dungeon_map = Dungeon.toPositive(dungeon_map)
+            dungeon_map = Dungeon.shuffleDungeon(dungeon_map)
+            all_valid = Dungeon.checkDungeon(dungeon_map, all_valid)
+            begin = Dungeon.findBegin(dungeon_map, begin)
+            print(dungeon_map)
+            Dungeon.print_ascii(dungeon_map)
+            all_valid = True
+    
+if __name__ == "__main__":
+    main()
